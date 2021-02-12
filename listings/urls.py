a97e51dc32c7b48a530fe72bae7d8ca5be2c9d14
rioumat <riou.mat@gmail.com>
2021-02-12 19:01:19 +0000

    pull data from listings model, display and add pagination

--- listings/urls.py
+++ listings/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='listings'),
-    path('<int:listing_id>', views.listing, name='listing'),
+    path('', views.index, name='listings'), # display the listing page using the index method defined in listings/views
+    path('<int:listing_id>', views.listing, name='listing'), # views.listing is the function in listing/views and int:listing_id is for the parameters
     path('search', views.search, name='search'),
 ]
 
--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from .models import Listing
 
 def index(request):
+    # import the listing ordered by date, the most recent is the first
+    # the filter link the admin page and the website
+    # show the house only if it is published
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-    return render(request, 'listings/listings.html')
+    # adding a paginator with 3 houses form the listings per page
+    paginator = Paginator(listings, 3)
+    # getting the page number
+    page = request.GET.get('page')
+    # passing the page to the paginator
+    paged_listings = paginator.get_page(page)
 
-def listing(request):
+    # dictionary of value we want to pass to the html page
+    # the html page needs to be updated in consequence
+    context = {
+        'listings': paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
