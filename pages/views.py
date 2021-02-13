93a40fbfc1f9b4e82dbacd67fd961b027c562997
rioumat <riou.mat@gmail.com>
2021-02-13 14:37:47 +0000

    correction of pagination, dynamic home page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
+
 def index(request):
-    return render(request, 'pages/index.html')
+    # adding the 3 most recent house to the main page
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
+
+    # add the realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # get all the mvp (seller of the month)
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
     return render(request, 'pages/about.html')
