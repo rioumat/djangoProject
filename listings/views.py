87043b2762681ff6700eb5e491c863329db7f23a
rioumat <riou.mat@gmail.com>
2021-02-15 17:30:06 +0000

    add a search page and search field

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
@@ -36,4 +37,45 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains = keywords) # looking if the description contains the keywords
+
+    # City
+    if 'city' in request.GET:
+        keywords = request.GET['city']
+        if keywords:
+            queryset_list = queryset_list.filter(city__iexact = keywords) # looking if the city field contains the exact keywords
+
+    # State
+    if 'state' in request.GET:
+        keywords = request.GET['state']
+        if keywords:
+            queryset_list = queryset_list.filter(state__iexact = keywords) # looking if the state field contains the exact keywords
+
+    # Bedroom
+    if 'bedrooms' in request.GET:
+        keywords = request.GET['bedrooms']
+        if keywords:
+            queryset_list = queryset_list.filter(bedrooms__lte = keywords) # looking if the bedroom field is less than keywords
+
+    # Price
+    if 'price' in request.GET:
+        keywords = request.GET['price']
+        if keywords:
+            queryset_list = queryset_list.filter(price__lte = keywords) # looking if the price field up to keywords
+
+
+    context = {
+        'state_choices': state_choices, # for the dynamic search field 
+        'bedroom_choices': bedroom_choices, 
+        'price_choices': price_choices,
+        'listings': queryset_list, # to do the search
+        'values': request.GET # to keep the search input into the field
+    }
+
+    return render(request, 'listings/search.html', context)
