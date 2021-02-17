93f75628ada55d24c06b51553a84f51d44c63a34
rioumat <riou.mat@gmail.com>
2021-02-17 19:14:52 +0000

    add contact app to be able to send email to the company

---
+++ contacts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from datetime import datetime
+
+# This model need to be added to the settings file in main project and then
+# migrated to the database using manage.py makemigrates contacts (name of the app)
+# and then run python3 manage.py migrate to add it ot the db
+
+class Contact(models.Model):
+    listing = models.CharField(max_length=200)
+    listing_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+    user_id = models.IntegerField(blank=True)
+    def __str__(self):
+        return self.name
+
+
