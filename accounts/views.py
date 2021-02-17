068c683cf11ddcc62615f7a2dfe67ef784a5dadd
rioumat <riou.mat@gmail.com>
2021-02-17 16:56:36 +0000

    add registering anf login functionnality

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,70 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 # those buttons are in the __navbar partial
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if password match
+        if password == password2:
+            # Check username (no duplicate)
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    # All good
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                    first_name=first_name, last_name=last_name)
+                    # Login after register
+                    #auth.login(request, user)
+                    #messages.success(request, 'You are now logged in')
+                    #return redirect('index')
+                    
+                    # user is being saved and he will need to login manually
+                    user.save()
+                    messages.success(request, 'You ae now registered and can log in')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password do not match') # Will disapear after 3s because of the javascript code in main.js
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
 def login(request):
     if request.method == 'POST':
-        return 
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
     
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
     
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
